f131bf3b0781a66bd80d0359be1de0c326cf1742
viniersiren <devinstuddard@gmail.com>
2024-12-21 02:58:33 -0800

    Initial Commit

---
+++ fetchTakeHome/fetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchTakeHomeApp.swift
+//  fetchTakeHome
+//
+//  Created by Devin Studdard on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct fetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
